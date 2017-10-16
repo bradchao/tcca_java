5e12da205fdf45efd7dd8329b7fe976aadd2dde4
User <User@NX>
2017-10-16 21:28:01 +0800

    v2

--- MyJava/src/tw/brad/java/example/Brad04.java
+++ MyJava/src/tw/brad/java/example/Brad04.java
@@ -10,6 +10,16 @@ public class Brad04 {
 		double d2 = 1.32e2;	// 1.32 x 10^2
 		System.out.println(d2);
 		
+		char c1 = 'a';
+		System.out.println(c1);
+		char c2 = 65;	// 0 ~ 65535 => 2^16
+		System.out.println(c2);
+		char c3 = '店';
+		System.out.println(c3);
+		
+		int a1 = c1 +120;
+		System.out.println(a1);
+		
 		
 	}
 
