89303750ad8e2fbb4b6bedcaf511b397ab887e8f
User <User@NX>
2017-10-16 20:50:49 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad02.java
@@ -0,0 +1,18 @@
+package tw.brad.java.example;
+
+public class Brad02 {
+
+	public static void main(String[] args) {
+		byte v1 = 1;
+		byte v2 = v1;
+		int v3 = v1 + v2;	// + - * / % => char,byte,short,int => int
+		byte v4 = 125;
+		v4++;
+		v4++;
+		v4++;
+		System.out.println(v4);
+		
+		
+	}
+
+}
