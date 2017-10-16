b387be8dfa271ba4538a5abcec7a8470b9b1a00c
User <User@NX>
2017-10-16 20:33:43 +0800

    v3

--- MyJava/src/tw/brad/java/example/Brad01.java
+++ MyJava/src/tw/brad/java/example/Brad01.java
@@ -1,10 +1,13 @@
 package tw.brad.java.example;
 
 public class Brad01 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte Var1;		// [a-zA-Z$_][a-zA-Z0-9$_]*
+		byte Var2 = 123;	// -128 ~ 127 => 2^8
+		byte Var3, Var4 = 12, Var5;
+		byte $_$;
+		byte 變數6;
+		
+		
 	}
-
 }
