0021ac40cceec69565073972808c794a128aaa05
User <User@NX>
2017-10-16 21:09:08 +0800

    v5

---
+++ MyJava/src/tw/brad/java/example/Brad03.java
@@ -0,0 +1,21 @@
+package tw.brad.java.example;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		byte v1 = 10;
+		short v2 = 3;
+		int v3 = v1 + v2;
+		long v4 = 123;
+		long v5 = v1 + v2 + v4;
+		
+		int a1 = 10, a2 = 0;
+		System.out.println("Brad");
+		int a3 = a1 / a2;
+		System.out.println(a3);
+		
+		
+		
+	}
+
+}
