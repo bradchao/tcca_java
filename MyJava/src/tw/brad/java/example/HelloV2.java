12fa6de635f163076dc6acc4cca70663aff01948
User <User@NX>
2017-10-16 20:14:59 +0800

    2017101601

---
+++ MyJava/src/tw/brad/java/example/HelloV2.java
@@ -0,0 +1,8 @@
+package tw.brad.java.example;
+
+public class HelloV2 {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ MyJava/src/tw/brad/java/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package tw.brad.java.example;
+
+public class HelloWorld {
+	/*
+	 * block 
+	 */
+	public static void main(String[] brad) {
+		System.out.println("Hello, World");	// My Hello, World
+		
+	}
+}
