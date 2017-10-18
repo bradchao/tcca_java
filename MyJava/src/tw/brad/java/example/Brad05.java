4c1def61b286fbc6ed41493d333f5a72ecfd71c1
User <User@NX>
2017-10-18 18:41:51 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad05.java
@@ -0,0 +1,15 @@
+package tw.brad.java.example;
+
+public class Brad05 {
+
+	public static void main(String[] args) {
+		boolean b1 = false;
+		if (b1) 
+			System.out.println("OK");
+		else 
+			System.out.println("XX");
+		
+		System.out.println("end");
+	}
+
+}
