4b7e1e220ac2393487e8854198aba7713c43cbc5
User <User@NX>
2017-10-18 21:26:02 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad12.java
@@ -0,0 +1,19 @@
+package tw.brad.java.example;
+
+public class Brad12 {
+
+	public static void main(String[] args) {
+		for (int j=0; j<=9; j++) {
+			for (int i=1; i<=10; i++) {
+				int n = i + j * 10;
+				
+				
+				
+				
+				System.out.print(n + "  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
