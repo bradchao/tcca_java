256ead20fed734713e4222df840ac84c06eb7b39
User <User@NX>
2017-10-18 19:51:17 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad08.java
@@ -0,0 +1,14 @@
+package tw.brad.java.example;
+
+public class Brad08 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		if (--a >= 10 && ++b <= 3) {
+			System.out.println("OK: a = " + a + "; b = " + b);
+		}else {
+			System.out.println("XX: a = " + a + "; b = " + b);
+		}
+	}
+
+}
