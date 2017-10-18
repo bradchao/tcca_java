d41bc6e9fa410a3746886209dd9e039b2141943c
User <User@NX>
2017-10-18 18:52:39 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad06.java
@@ -0,0 +1,15 @@
+package tw.brad.java.example;
+
+public class Brad06 {
+
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		int score = 76;
+		if (score >= 60) {
+			System.out.println("PASS");
+		}else {
+			System.out.println("DOWN");
+		}
+	}
+
+}
