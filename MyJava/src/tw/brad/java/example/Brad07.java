450257a39edc8a71a5e6fe4a0165045ef58c3888
User <User@NX>
2017-10-18 19:43:24 +0800

    v3

--- MyJava/src/tw/brad/java/example/Brad07.java
+++ MyJava/src/tw/brad/java/example/Brad07.java
@@ -6,7 +6,19 @@ public class Brad07 {
 	public static void main(String[] args) {
 		String yearString = JOptionPane.showInputDialog("Input year");
 		int year = Integer.parseInt(yearString);
-		System.out.println(year + 1);
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					// 29
+				}else {
+					// 28
+				}
+			}else {
+				// 29
+			}
+		}else {
+			// 28
+		}
 		
 		
 	}
