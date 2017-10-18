391ee74dbf1870c7e36c5a5c19c7445c9fe6834b
User <User@NX>
2017-10-18 21:11:09 +0800

    v4

--- MyJava/src/tw/brad/java/example/Brad99.java
+++ MyJava/src/tw/brad/java/example/Brad99.java
@@ -3,12 +3,16 @@ package tw.brad.java.example;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		for (int y=1; y<=9; y++) {
-			for (int x = 2; x<=5; x++) {
-				int r = x * y;
-				System.out.print(x + " x " + y + " = " + r + "\t");
+		for (int z=0; z<4; z++) {
+			for (int y=1; y<=9; y++) {
+				for (int x = 2; x<=5; x++) {
+					int newx = x + z*4;
+					int r = newx * y;
+					System.out.print(newx + " x " + y + " = " + r + "\t");
+				}
+				System.out.println();
 			}
-			System.out.println();
+			System.out.println("----");
 		}
 	}
 
