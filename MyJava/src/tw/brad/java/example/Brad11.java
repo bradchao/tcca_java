9edaf9a8e16c6d917c8a169df70a0824580e5d09
User <User@NX>
2017-10-18 20:52:11 +0800

    v1

--- MyJava/src/tw/brad/java/example/Brad11.java
+++ MyJava/src/tw/brad/java/example/Brad11.java
@@ -3,8 +3,10 @@ package tw.brad.java.example;
 public class Brad11 {
 
 	public static void main(String[] args) {
-		for (int i = 0; i<10; i++) {
+		int i = 0;
+		for ( System.out.println("Brad"); ; System.out.println("----")) {
 			System.out.println(i);
+			i++;
 		}
 	}
 
---
+++ MyJava/src/tw/brad/java/example/Brad99.java
@@ -0,0 +1,12 @@
+package tw.brad.java.example;
+
+public class Brad99 {
+
+	public static void main(String[] args) {
+		for (int x = 2; x<=5; x++) {
+			int r = x * 1;
+			System.out.print(x + " x 1 = " + r + "\t");
+		}
+	}
+
+}
