977d4895ad3258ba644659535e22762e57485954
User <User@NX>
2017-10-20 19:01:25 +0800

    v4

--- MyJava/src/tw/brad/java/example/Brad13.java
+++ MyJava/src/tw/brad/java/example/Brad13.java
@@ -4,7 +4,6 @@ public class Brad13 {
 
 	public static void main(String[] args) {
 		int[] a1;	// good
-		int a2[];
 		a1 = new int[3];
 		System.out.println(a1.length);
 		System.out.println(a1[0]);
@@ -21,6 +20,15 @@ public class Brad13 {
 		for (int v : a1) {
 			System.out.println(v);
 		}
+		
+		int[] a2 = new int[]{1,2,3,4};
+		int[] a3 = {1,2,3,4};
+		int[] a4;
+		int[] a5;
+		a4 = new int[] {1,2,3,4};
+		//a5 = {1,2,3,4};
+		
+		
 		
 		
 		
