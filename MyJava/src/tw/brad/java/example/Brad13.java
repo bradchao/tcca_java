d4d3e455db880939ff104471edfe78407a322108
User <User@NX>
2017-10-20 18:44:57 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad13.java
@@ -0,0 +1,18 @@
+package tw.brad.java.example;
+
+public class Brad13 {
+
+	public static void main(String[] args) {
+		int[] a1;	// good
+		int a2[];
+		a1 = new int[3];
+		System.out.println(a1.length);
+		System.out.println(a1[0]);
+		System.out.println(a1[1]);
+		System.out.println(a1[2]);
+		//System.out.println(a1[3]);
+		
+		
+	}
+
+}
