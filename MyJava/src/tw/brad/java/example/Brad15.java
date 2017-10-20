2ee36482050da151c19c64d5345a0a6c47461f61
User <User@NX>
2017-10-20 19:45:08 +0800

    v1

---
+++ MyJava/src/tw/brad/java/example/Brad15.java
@@ -0,0 +1,16 @@
+package tw.brad.java.example;
+
+public class Brad15 {
+	public static void main(String[] args) {
+		int[][] a1;
+		int[] a2[];
+		int a3[][];
+		
+		a1 = new int[3][4];
+		a2 = new int[3][];
+		a2[0] = new int[4];
+		a2[1] = new int[2];
+		a2[2] = new int[5];
+		
+	}
+}
