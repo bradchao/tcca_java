ebae8d936ff3d3c0bf0bfb32362492391e8c65bc
User <User@NX>
2017-10-23 21:26:34 +0800

    2017102310

--- MyJava/src/tw/brad/java/example/Brad23.java
+++ MyJava/src/tw/brad/java/example/Brad23.java
@@ -1,11 +1,45 @@
 package tw.brad.java.example;
 
+import javax.swing.JOptionPane;
+
 public class Brad23 {
 	public static void main(String[] args) {
 		// 產生謎底
 		String answer = createAnswer(3);
-		System.out.println(answer);
+		//System.out.println(answer);
+
+		boolean isWINNER = false;
+		for (int i=0; i<10; i++) {
+			String guess = JOptionPane.showInputDialog("Input a number");
+			String result = checkAB(answer, guess);
+			JOptionPane.showMessageDialog(null, result);
+			
+			if (result.equals("3A0B")) {
+				JOptionPane.showMessageDialog(null, "WINNER");
+				isWINNER = true;
+				break;
+			}
+		}
 		
+		if (!isWINNER) {
+			JOptionPane.showMessageDialog(null, "Loser");
+		}
+		
+		
+	}
+	
+	static String checkAB(String a, String g) {
+		int A, B; A = B = 0;
+		for (int i=0; i<g.length(); i++) {
+			char gc = g.charAt(i); 
+			if (gc == a.charAt(i)) {
+				A++;
+			}else if (a.indexOf(gc)>=0) {
+				B++;
+			}
+		}
+		
+		return A + "A" + B + "B";
 	}
 	
 	static String createAnswer(int d) {
