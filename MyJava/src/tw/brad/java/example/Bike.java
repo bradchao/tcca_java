56fa3c96c5f60baa7cf0345e0d8051dce2107012
User <User@NX>
2017-10-23 19:35:55 +0800

    2017102307

--- MyJava/src/tw/brad/java/example/Bike.java
+++ MyJava/src/tw/brad/java/example/Bike.java
@@ -1,6 +1,21 @@
 package tw.brad.java.example;
 
 public class Bike {
-	double speed;
+	private double speed;
 	
+	void upSpeed() {
+		speed = speed<1?1:speed*1.2;
+	}
+	
+	void upSpeed(int gear) {
+		speed = speed<1?1:speed*(1.2+gear);
+	}
+	
+	void downSpeed() {
+		speed = speed<1?0:speed*0.7;
+	}
+	
+	double getSpeed() {
+		return speed;
+	}
 }
--- MyJava/src/tw/brad/java/example/Brad18.java
+++ MyJava/src/tw/brad/java/example/Brad18.java
@@ -4,7 +4,24 @@ public class Brad18 {
 
 	public static void main(String[] args) {
 		Bike b1 = new Bike();
-		System.out.println(b1.speed);
+		System.out.println(b1.getSpeed());
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		System.out.println(b1.getSpeed());
+		b1.downSpeed();
+		b1.downSpeed();
+		b1.downSpeed();
+		b1.downSpeed();
+		b1.downSpeed();
+		System.out.println(b1.getSpeed());
+		
+		Bike b2 = new Bike();
+		b2.upSpeed(4);
+		b2.upSpeed(3);
+		System.out.println(b2.getSpeed());
+		
+		
 	}
 
 }
