6de213ca12e5fd1eb2ad8f68e3bc90539b76d66b
User <User@NX>
2017-10-23 18:36:45 +0800

    2017102301

---
+++ MyJava/src/tw/brad/java/example/Brad16.java
@@ -0,0 +1,12 @@
+package tw.brad.java.example;
+
+public class Brad16 {
+	public static void main(String[] args) {
+		int a = 10;
+		while (a>0) {
+			System.out.println(a);
+			a--;
+		}
+		
+	}
+}
