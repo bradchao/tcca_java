968f6bff28e1bdaba427c5836a83a787381b1244
User <User@NX>
2017-10-25 20:03:20 +0800

    2017102508

--- MyJava/src/tw/brad/java/example/TWId.java
+++ MyJava/src/tw/brad/java/example/TWId.java
@@ -3,8 +3,20 @@ package tw.brad.java.example;
 public class TWId {
 	private String id;
 	public TWId() {
+		this((int)(Math.random()*2)==0);
+	}
+	public TWId(boolean isMale) {
+		this(isMale, (int)(Math.random()*26));
+	}
+	public TWId(int area) {
+		this((int)(Math.random()*2)==0 ,area);
+	}
+	public TWId(boolean isMale, int area) {
 		
 	}
+	
+	
+	
 	public TWId(String id) {
 		this.id = id;
 	}
