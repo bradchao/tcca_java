a6365b1ed7103fe70af6c567b9a7fe30d7fb8dc5
User <User@NX>
2017-10-25 20:40:56 +0800

    2017102510

--- MyJava/src/tw/brad/java/example/Brad26.java
+++ MyJava/src/tw/brad/java/example/Brad26.java
@@ -3,7 +3,7 @@ package tw.brad.java.example;
 public class Brad26 {
 
 	public static void main(String[] args) {
-		Brad261 obj1 = new Brad261(2f);
+		Brad261 obj1 = new Brad261(2,3);
 	}
 
 }
@@ -15,7 +15,19 @@ class Brad261 {
 //	Brad261(int a){
 //		System.out.println("Brad261(int)");
 //	}
+	Brad261(long a){
+		System.out.println("Brad261(long)");
+	}
+	Brad261(float a){
+		System.out.println("Brad261(float)");
+	}
 	Brad261(double a){
 		System.out.println("Brad261(double)");
+	}
+//	Brad261(float a, double b){
+//		System.out.println("Brad261(float,double)");
+//	}
+	Brad261(double a, float b){
+		System.out.println("Brad261(double,float)");
 	}
 }
