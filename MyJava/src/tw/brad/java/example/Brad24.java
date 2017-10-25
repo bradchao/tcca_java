51d45fbef8315906b9083d9d0aeb6898fe7bb66d
User <User@NX>
2017-10-25 18:43:25 +0800

    2017102501

---
+++ MyJava/src/tw/brad/java/example/Brad24.java
@@ -0,0 +1,12 @@
+package tw.brad.java.example;
+
+public class Brad24 {
+
+	public static void main(String[] args) {
+		TWId id1 = new TWId("A123456789");
+		if (id1.isMale()) {
+			
+		}
+	}
+
+}
---
+++ MyJava/src/tw/brad/java/example/TWId.java
@@ -0,0 +1,19 @@
+package tw.brad.java.example;
+
+public class TWId {
+	private String id;
+	public TWId() {
+		
+	}
+	public TWId(String id) {
+		
+	}
+	public boolean isMale() {
+		return true;
+	}
+	public String getArea() {
+		return "A";
+	}
+	
+	
+}
