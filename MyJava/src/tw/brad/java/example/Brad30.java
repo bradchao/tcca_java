cb9fc5311e74e10a488ae40e2f1f442b922b5478
User <User@NX>
2017-10-27 20:31:37 +0800

    2017102706

--- MyJava/src/tw/brad/java/example/Brad30.java
+++ MyJava/src/tw/brad/java/example/Brad30.java
@@ -1,11 +1,27 @@
 package tw.brad.java.example;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Brad30 extends JFrame {
+	// Brad30 Object has-a JButton Object
+	// Brad30 Object is-a JFrame, .... Container
+	private JButton open, save, exit;
+	
 	public Brad30() {
 		// super();
 		super("視窗城市");
+		FlowLayout layout = new FlowLayout();
+		setLayout(layout);
+		
+		open = new JButton("Open");
+		save = new JButton("Save");
+		exit = new JButton("Exit");
+		
+		add(open); add(save); add(exit);
+		
 		setSize(640, 480);
 		setVisible(true);
 	}
