b1fefd9aa24d20e7707ea2b10aa275b9b183f6fc
User <User@NX>
2017-11-01 20:08:34 +0800

    201711012008

--- MyJava/src/tw/brad/java/example/Brad44.java
+++ MyJava/src/tw/brad/java/example/Brad44.java
@@ -11,6 +11,11 @@ public class Brad44 {
 		try {
 			File file = new File("./dir1/file1");
 			FileOutputStream fout = new FileOutputStream(file);
+			String data = "Hello, World\r\n1234567\r\n7654321\r\n";
+			fout.write(data.getBytes());
+			String data2 = "abcdefg\r\n";
+			fout.write(data2.getBytes());
+			fout.flush();
 			fout.close();
 			System.out.println("OK");
 		}catch(FileNotFoundException fe) {
