9b39a7ec2b255f7201fabf18052bdcf18fddb15f
User <User@NX>
2017-11-01 19:58:34 +0800

    201711011958

---
+++ MyJava/src/tw/brad/java/example/Brad44.java
@@ -0,0 +1,24 @@
+package tw.brad.java.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Brad44 {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("./dir1/file1");
+			FileOutputStream fout = new FileOutputStream(file);
+			fout.close();
+			System.out.println("OK");
+		}catch(FileNotFoundException fe) {
+			System.out.println("not found");
+		}catch(IOException fe) {
+			System.out.println("i/o eception");
+		}
+		
+	}
+
+}
