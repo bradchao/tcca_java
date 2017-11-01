a4396a5045089de9a78bc4ffd70ffd76deca6af2
User <User@NX>
2017-11-01 20:34:12 +0800

    201711012034

--- MyJava/src/tw/brad/java/example/Brad45.java
+++ MyJava/src/tw/brad/java/example/Brad45.java
@@ -7,8 +7,13 @@ public class Brad45 {
 	public static void main(String[] args) {
 		try {
 			FileInputStream fin = new FileInputStream("./dir1/file1");
+			int v1;
+			while ( (v1 = fin.read()) != -1) {
+				System.out.print((char)v1);
+			}
+			
 			fin.close();
-			System.out.println("OK");
+			//System.out.println("OK");
 		} catch (FileNotFoundException e) {
 			System.out.println("E1:" +e.toString());
 		} catch (IOException e) {
