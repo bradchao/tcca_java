d7d862aacf46f827e527d9305cdc7a9c1647fa36
User <User@NX>
2017-11-06 19:46:44 +0800

    201711061946

--- MyJava/src/tw/brad/java/example/Brad62.java
+++ MyJava/src/tw/brad/java/example/Brad62.java
@@ -1,5 +1,6 @@
 package tw.brad.java.example;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -12,12 +13,21 @@ public class Brad62 {
 			ServerSocket server = new ServerSocket(9999);
 			Socket socket = server.accept();
 			InputStream in = socket.getInputStream();
-			int i; String str = "";
-			while ( (i = in.read()) != -1) {
-				str += (char)i;
+			
+			FileOutputStream fout = new FileOutputStream("./upload/brad.jpg");
+
+			byte[] buf = new byte[4096]; int len = -1;
+			while ( (len = in.read(buf)) != -1) {
+				fout.write(buf, 0, len);
 			}
+			
+			fout.flush();
+			fout.close();
+			
+			in.close();
+			
 			server.close();
-			System.out.println("OK:" + str);
+			System.out.println("OK:from " + socket.getInetAddress().getHostAddress());
 		} catch (IOException e) {
 			System.out.println("ERR:" +e.toString());
 		}
--- MyJava/src/tw/brad/java/example/Brad63.java
+++ MyJava/src/tw/brad/java/example/Brad63.java
@@ -1,5 +1,7 @@
 package tw.brad.java.example;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
@@ -9,9 +11,15 @@ public class Brad63 {
 
 	public static void main(String[] args) {
 		try {
+			File file = new File("./dir1/coffee.jpg");
+			byte[] buf = new byte[(int)(file.length())];
+			FileInputStream fin = new FileInputStream(file);
+			fin.read(buf);
+			fin.close();
+			
 			Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 9999);
 			OutputStream out = socket.getOutputStream();
-			out.write("Hello, TCP".getBytes());
+			out.write(buf);
 			out.flush();
 			out.close();
 		} catch (IOException e) {
