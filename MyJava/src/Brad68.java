e1752ee1f8bd437c241f6809f317587727b2ff84
User <User@NX>
2017-11-08 19:57:50 +0800

    201711081957

--- MyJava/src/Brad68.java
+++ MyJava/src/Brad68.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Brad68 {
 
@@ -9,8 +10,10 @@ public class Brad68 {
 			
 			Connection conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost/tcca?user=root&password=root");
-			
-			
+			Statement stmt = conn.createStatement();
+			int i = stmt.executeUpdate(
+					"insert into cust (cname,tel,birthday) values ('brad','123','1999-01-02')");
+			System.out.println(i);
 		} catch (Exception e) {
 			System.out.println("Exception");
 		}
