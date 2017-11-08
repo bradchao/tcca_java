719219b0ec10442f828bbbc03d7f26a127cf759f
User <User@NX>
2017-11-08 20:06:56 +0800

    201711082006

--- MyJava/src/Brad68.java
+++ MyJava/src/Brad68.java
@@ -12,7 +12,11 @@ public class Brad68 {
 					"jdbc:mysql://localhost/tcca?user=root&password=root");
 			Statement stmt = conn.createStatement();
 			int i = stmt.executeUpdate(
-					"insert into cust (cname,tel,birthday) values ('brad','123','1999-01-02')");
+					"insert into cust (cname,tel,birthday) values " + 
+			"('brad1','123','1999-01-02'), " +
+			"('brad2','123','1999-01-02'), " +
+			"('brad3','123','1999-01-02') " 
+			);
 			System.out.println(i);
 		} catch (Exception e) {
 			System.out.println("Exception");
