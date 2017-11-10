07030d6a9df7217d4093de5e3ff88e79d0627841
User <User@NX>
2017-11-10 21:22:33 +0800

    201711102122

--- MyJava/src/tw/brad/java/example/Brad77.java
+++ MyJava/src/tw/brad/java/example/Brad77.java
@@ -1,6 +1,7 @@
 package tw.brad.java.example;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class Brad77 {
 
@@ -8,7 +9,19 @@ public class Brad77 {
 		HashMap map = new HashMap();
 		map.put("name", "Brad");
 		map.put("weight", 82.5);
-		System.out.println(map.get("weight"));
+		map.put("gender", false);
+		map.put("age", 52);
+		
+		map.put("weight", 80);
+		
+		Set set = map.keySet();
+		for(Object key :set) {
+			System.out.println(key + " => " + map.get(key));
+		}
+		
+		
+		
+		
 	}
 
 }
