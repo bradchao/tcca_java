4c32b4710fd6a0c857c020c3b5ff535591e57ac1
User <User@NX>
2017-11-10 20:34:27 +0800

    201711102034

--- MyJava/src/tw/brad/java/example/Brad74.java
+++ MyJava/src/tw/brad/java/example/Brad74.java
@@ -3,29 +3,32 @@ package tw.brad.java.example;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class Brad73 {
+public class Brad74 {
 
 	public static void main(String[] args) {
 		Integer i1 = 123;
 		Integer i2 = 124;
 		System.out.println(i1.hashCode());
 		System.out.println(i2.hashCode());
-		HashSet set = new HashSet();
+		HashSet<Integer> set = new HashSet<>();
 		while (set.size()<6) {
 			set.add((int)(Math.random()*49+1));
 		}
+		//set.add("abcd");
+		
 		System.out.println(set.toString());
-		Iterator it = set.iterator();
+		Iterator<Integer> it = set.iterator();
 		while (it.hasNext()) {
-			Object obj = it.next();
-			Integer ii = (Integer)obj;
+			Integer ii = it.next();
 			System.out.println(ii);
 		}
+		
 		System.out.println("---");
+		
 		// Collection
-		for (Object i :set ) {
-			Integer ii = (Integer)i;
-			System.out.println(ii);
+		for (Integer i :set ) {
+			//Integer ii = (Integer)i;
+			System.out.println(i);
 		}
 		
 		
