689151c3f8f1384814b46399990a6becf3bf05e6
User <User@NX>
2017-11-13 19:13:17 +0800

    201711131913

--- MyJava/src/tw/brad/java/example/MyPanel.java
+++ MyJava/src/tw/brad/java/example/MyPanel.java
@@ -1,7 +1,31 @@
 package tw.brad.java.example;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
-
+	public MyPanel() {
+		setBackground(Color.YELLOW);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		//System.out.println("Here");
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(4));
+		g2d.drawLine(0, 0, 200, 200);
+		
+		
+		
+		
+	}
+	
 }
