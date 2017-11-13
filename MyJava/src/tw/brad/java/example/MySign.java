3e7f35138305cfbada55a28aef37d4a94ab3fd61
User <User@NX>
2017-11-13 21:27:58 +0800

    201711132127

--- MyJava/src/tw/brad/java/example/MySign.java
+++ MyJava/src/tw/brad/java/example/MySign.java
@@ -4,17 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MySign extends JFrame {
-	private JButton clear, undo, redo, save, open;
+	private JButton clear, undo, redo, save, open, saveJpeg;
 	private MyPanel myPanel;
 	
 	public MySign() {
@@ -27,7 +33,9 @@ public class MySign extends JFrame {
 		redo = new JButton("Redo");
 		save = new JButton("Save");
 		open = new JButton("Open");
+		saveJpeg = new JButton("Save Jpeg");
 		top.add(clear);top.add(undo);top.add(redo);top.add(save);top.add(open);
+		top.add(saveJpeg);
 		
 		add(top, BorderLayout.NORTH);
 		
@@ -69,6 +77,13 @@ public class MySign extends JFrame {
 			}
 		});
 		
+		saveJpeg.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveJPEG();
+			}
+		});
+		
 		setSize(800, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -102,6 +117,17 @@ public class MySign extends JFrame {
 		}catch(Exception e) {
 			System.out.println("ERR:" + e.toString());
 		}
+	}
+	
+	private void saveJPEG() {
+		BufferedImage img = new BufferedImage(myPanel.getWidth(), myPanel.getHeight(), 
+				BufferedImage.TYPE_INT_RGB);
+		myPanel.print(img.getGraphics()); // or: panel.printAll(...);
+		try {
+	        ImageIO.write(img, "jpg", new File("./dir1/panel.jpg"));
+	    }catch (IOException e) {
+	    }
+		
 	}
 	
 	public static void main(String[] args) {
