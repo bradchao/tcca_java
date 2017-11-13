ce30f95b5fcbc471ce2abf198d03d7aa29d36ba0
User <User@NX>
2017-11-13 19:30:16 +0800

    201711131930

--- MyJava/src/tw/brad/java/example/MyPanel.java
+++ MyJava/src/tw/brad/java/example/MyPanel.java
@@ -4,12 +4,19 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
 	public MyPanel() {
 		setBackground(Color.YELLOW);
+		
+		MyMouseListener listener = new MyMouseListener();
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
+		
 	}
 	
 	@Override
@@ -22,10 +29,29 @@ public class MyPanel extends JPanel {
 		g2d.setColor(Color.BLUE);
 		g2d.setStroke(new BasicStroke(4));
 		g2d.drawLine(0, 0, 200, 200);
-		
-		
-		
+	}
+	
+	private class MyMouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			super.mouseDragged(e);
+			System.out.println("drag");
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			super.mousePressed(e);
+			System.out.println("press");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			super.mouseReleased(e);
+			System.out.println("release");
+		}
 		
 	}
+	
 	
 }
