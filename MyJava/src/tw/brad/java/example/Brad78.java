182035262408221b1438304213364492d5cc8b39
User <User@NX>
2017-11-15 18:55:58 +0800

    201711151855

--- MyJava/src/tw/brad/java/example/Brad78.java
+++ MyJava/src/tw/brad/java/example/Brad78.java
@@ -3,8 +3,21 @@ package tw.brad.java.example;
 public class Brad78 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyThread mt1 = new MyThread();
+		MyThread mt2 = new MyThread();
+		mt1.start();
+		mt2.start();
+		
 	}
 
+	
 }
+class MyThread extends Thread {
+	@Override
+	public void run() {
+		for (int i=0; i<20; i++) {
+			System.out.println("i = " + i);
+		}
+	}
+}
+
