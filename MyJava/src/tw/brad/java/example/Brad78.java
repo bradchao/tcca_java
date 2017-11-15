db9cb5401c362313de2070f270839b49c4c80ac8
User <User@NX>
2017-11-15 19:11:15 +0800

    201711151911

--- MyJava/src/tw/brad/java/example/Brad78.java
+++ MyJava/src/tw/brad/java/example/Brad78.java
@@ -3,8 +3,8 @@ package tw.brad.java.example;
 public class Brad78 {
 
 	public static void main(String[] args) {
-		MyThread mt1 = new MyThread();
-		MyThread mt2 = new MyThread();
+		MyThread mt1 = new MyThread("A");
+		MyThread mt2 = new MyThread("B");
 		mt1.start();
 		mt2.start();
 		//mt1.start();
@@ -14,10 +14,12 @@ public class Brad78 {
 }
 
 class MyThread extends Thread {
+	private String name;
+	MyThread(String name){this.name = name;}
 	@Override
 	public void run() {
 		for (int i=0; i<20; i++) {
-			System.out.println("i = " + i);
+			System.out.println(name + " = " + i);
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
